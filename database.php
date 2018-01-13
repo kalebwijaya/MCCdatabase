5357668b3ef9e19e1752325cfd202cd42c6fe1f1
kalebwijaya <wijayakaleb@gmail.com>
2018-01-13 16:46:30 +0700

    Update database.php

--- database.php
+++ database.php
@@ -1,26 +1,31 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-$server = "sql12.freemysqlhosting.net";
-$name = "sql12214609";
-$username = "sql12214609";
-$password = "tpkdEAGJeb";
+	header("Access-Control-Allow-Origin: *");
+	$server = "sql12.freemysqlhosting.net";
+	$name = "sql12214609";
+	$username = "sql12214609";
+	$password = "tpkdEAGJeb";
 
-$connection = mysqli_connect($server,$username,$password,$name);
+	$connection = mysqli_connect($server,$username,$password,$name);
 
-$query_result = $connection->query("SELECT * FROM `books table`");
+	if ($_REQUEST) {
+		$search_query = $_REQUEST['q'];
+		$query_result = $connection->query("SELECT * FROM `books table` WHERE title LIKE '%$search_query%' ");
+	}else{
+		$query_result = $connection->query("SELECT * FROM `books table`");
+	}
 
-$books = [];
-$counter = 0;
-$book = new stdClass();
+	$books = [];
+	$counter = 0;
+	$book = new stdClass();
 
-while($data = $query_result->fetch_assoc()){
-	$books[] = $data;
-	$counter = $counter +1;
-}
+	while($data = $query_result->fetch_assoc()){
+		$books[] = $data;
+		$counter = $counter +1;
+	}
 
-$book->counts = $counter;
-$book->books = $books;
+	$book->counts = $counter;
+	$book->books = $books;
 
-header('Content-Type: application/json');
-print json_encode($book);
+	header('Content-Type: application/json');
+	print json_encode($book);
 ?>
